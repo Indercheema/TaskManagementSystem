8dbeeb7eadd21c2e76e7f6cf03cfbe7bc53d1740
inder <cheema.inder10@gmail.com>
2023-04-10 11:12:46 -0500

    Project, Task Models added

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using TaskManagementSystem.Models;
+using Task = TaskManagementSystem.Models.Task;
 
 namespace TaskManagementSystem.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    [Display(Name = "Full Name")]
+
+    public string FullName { get; set; }
+    public virtual HashSet<ProjectContributor> ProjectContributors { get; set; } = new HashSet<ProjectContributor>();
+
+    public virtual HashSet<Task> Tasks { get; set; } = new HashSet<Task>();
+
 }
 
