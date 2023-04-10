54e45612a5c101fe08ecc14b2bf0cea4d75aa6c9
inder <cheema.inder10@gmail.com>
2023-04-10 12:51:23 -0500

    Added Task and Project Models

--- Models/ProjectContributor.cs
+++ Models/ProjectContributor.cs
@@ -1,6 +1,30 @@
-﻿namespace TaskManagementSystem.Models
+﻿using TaskManagementSystem.Areas.Identity.Data;
+
+namespace TaskManagementSystem.Models
 {
     public class ProjectContributor
     {
+        public int Id { get; set; }
+
+        public Project? Project { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public string UserId { get; set; }
+
+        public ProjectContributor()
+        {
+
+        }
+
+        public ProjectContributor( Project project, ApplicationUser applicationUser)
+        {
+            Project = project;
+            ProjectId = project.Id;
+            ApplicationUser = applicationUser;
+            UserId = applicationUser.Id;
+        }
     }
 }
