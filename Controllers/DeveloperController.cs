85a2f54c0d739a24aaf1ae019429e65216f516d2
inder <cheema.inder10@gmail.com>
2023-04-15 14:43:43 -0500

    Developer Index View Updated

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -53,7 +53,6 @@ namespace TaskManagementSystem.Controllers
                 vm.ProjectContributors = projects;
             }
 
-
             vm.Tasks = AllTasksInProjects.Skip((page - 1) * 2).Take(2).ToHashSet();
 
 
